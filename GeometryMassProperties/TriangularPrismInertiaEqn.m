f6bff7a6c9c3780d8dc5c0a4297c30bc4ca2f5a6
Emily Bates <batesemily97@gmail.com>
2024-04-09 22:16:50 -0700

    updates to inertia tensor calc

--- GeometryMassProperties/TriangularPrismInertiaEqn.m
+++ GeometryMassProperties/TriangularPrismInertiaEqn.m
@@ -1,17 +1,19 @@
 syms a b z h m p
 
-p  = (2*m)/(a*h*b);
+p  = (2*m)/(a*h*b); % density
 dm = -((p*a*b)/h) * (z-h);
-x  = ((-z*a)/h) + a;
+x  = ((-z*a)/h) + (2/3)*a;
 
-dIx = ((dm * b^2)/12) + (dm * (z^2 + (b/2)^2));
-Ix  = int(dIx, z, 0, h);
+% set CM at origin and do integrals w/ limits reflecting that
+
+dIx = ((dm * b^2)/12) + (dm * (z^2 )); % use distance from slice CM to prism CM
+Ix  = int(dIx, z, -h/3, 2*h/3);
 
 dIy = ((dm * x^2)/12) + (dm * (z^2 + (x/2)^2));
-Iy  = int(dIy, z, 0, h);
+Iy  = int(dIy, z, -h/3, 2*h/3);
 
-dIz = (dm * (x^2 + b^2)/12) + (dm * ((b/2)^2 + (x/2)^2));
-Iz  = int(dIz, z, 0, h);
+dIz = (dm * (x^2 + b^2)/12) + (dm * (  (x/2)^2));
+Iz  = int(dIz, z, -h/3, 2*h/3);
  
 pretty(simplify(Ix));
 pretty(simplify(Iy));
