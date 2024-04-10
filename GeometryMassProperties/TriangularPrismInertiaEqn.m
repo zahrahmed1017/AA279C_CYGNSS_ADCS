35fed671bd3b0f3df0c34512ee5007a896d7612f
Zahra Ahmed <zahra.ahmed1017@gmail.com>
2024-04-10 00:45:58 -0700

    THE INERTIA TENSOR IS CORRECT HALLELUJAH

--- GeometryMassProperties/TriangularPrismInertiaEqn.m
+++ GeometryMassProperties/TriangularPrismInertiaEqn.m
@@ -1,18 +1,18 @@
-syms a b z h m p
+syms a b z h m
 
 p  = (2*m)/(a*h*b); % density
-dm = -((p*a*b)/h) * (z-h);
-x  = ((-z*a)/h) + (2/3)*a;
+x  = ((-z*a)/h) + (a/3);
+dm = p * (x + (a/3)) * b;
 
 % set CM at origin and do integrals w/ limits reflecting that
 
-dIx = ((dm * b^2)/12) + (dm * (z^2 )); % use distance from slice CM to prism CM
+dIx = ((dm * b^2)/12) + (dm * z^2); % use distance from slice CM to prism CM
 Ix  = int(dIx, z, -h/3, 2*h/3);
 
-dIy = ((dm * x^2)/12) + (dm * (z^2 + (x/2)^2));
+dIy = ((dm * (x + (a/3))^2)/12) + (dm * (z^2 + ((a/6) - (x/2))^2));
 Iy  = int(dIy, z, -h/3, 2*h/3);
 
-dIz = (dm * (x^2 + b^2)/12) + (dm * (  (x/2)^2));
+dIz = (dm * ((x + a/3)^2 + b^2)/12) + (dm * ((a/6) - (x/2))^2);
 Iz  = int(dIz, z, -h/3, 2*h/3);
  
 pretty(simplify(Ix));
