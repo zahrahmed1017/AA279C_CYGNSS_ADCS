ce388ebd9b4996aa9901ec06772cf7c4e1fd6b40
Zahra Ahmed <zahra.ahmed1017@gmail.com>
2024-04-22 14:23:03 -0700

    Updating quaternion kinematics part 2

--- Simulation_and_Propagation/QuaternionKinematics.m
+++ Simulation_and_Propagation/QuaternionKinematics.m
@@ -17,9 +17,7 @@ Om = [0,  wz, -wy,  wx;...
       wy, -wx, 0,   wz;...
      -wx, -wy, -wz, 0];
 
-q_dot_temp = 0.5 .* Om * [q1;q2;q3;q4];
+q_dot = 0.5 .* Om * [q1;q2;q3;q4];
 
-% Normalize the quaternion to unit length
-q_dot = q_dot_temp ./ norm(q_dot_temp);
 
 end
