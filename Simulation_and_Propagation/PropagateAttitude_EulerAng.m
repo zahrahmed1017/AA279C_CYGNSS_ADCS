f0d83c9bd7b4e1af630cd6880843611955700d55
Emily Bates <batesemily97@gmail.com>
2024-04-22 15:46:58 -0700

    euler angle propagation

--- Simulation_and_Propagation/PropagateAttitude_EulerAng.m
+++ Simulation_and_Propagation/PropagateAttitude_EulerAng.m
@@ -1,20 +1,16 @@
-function qw_d = PropagateAttitude_Quat(qw, M_vec, I_p)
+function ang_w_d = PropagateAttitude_Quat(ang_w, M_vec, I_p)
 
-% qw --> [q1 q2 q3 q4 w1 w2 w3]
+% ang_w --> [phi theta psi w1 w2 w3]
+% euler angles correspond to 3-1-3 sequence, in radians
 
-% Not sure if I can actually do it this way.....was trying to just make one
-% equation to propagate the dynamics (Euler) and then kinematics
-% (w quaternions) in one function. But we need to integrate the dynamics at
-% every time step and pass that to kinematics?
+angs = ang_w(1:3);
+w = ang_w(4:6);
 
-q = qw(1:4);
-w = qw(5:7);
-
-q_d = QuaternionKinematics(q,w);
+angs_d = EulerAngKinematics(angs,w);
 
 w_d = PropagateEuler(w, M_vec, I_p);
 
-qw_d = [q_d; w_d];
+ang_w_d = [angs_d; w_d];
 
 end
 
