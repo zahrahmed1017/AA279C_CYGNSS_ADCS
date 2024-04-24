e4948649e987381f7952e0a165692d7932e80c83
Emily Bates <batesemily97@gmail.com>
2024-04-24 14:46:08 -0700

    plotting inertial ang. momentum and velocity

---
+++ Simulation_and_Propagation/Inertial_Rotation_Quantities.m
@@ -0,0 +1,50 @@
+%% Load data
+
+close all; clear;
+
+load PropAttitude_Quat_Data.mat
+load InertiaData.mat
+
+%% Compute angular velocity and momentum in inertial coordinates
+
+% Get angular momentum , expressed in PA coordinates 
+w_i_pa = qw_prop(:,5:7)'; % quantity in inertial frame, but expressed in PA
+% coordinates
+
+% This should multiply each column in the 3xN time series array by the matrix, 
+% returning another 3xN time series array
+L_i_pa = I_p * w_i_pa; 
+
+[n, ~] = size(qw_prop);
+L_i_i = zeros(3, n);
+w_i_i = zeros(3, n);
+for i = 1:n
+
+    q_i_pa = qw_prop(i,1:4)'; % attitude quaternion for this time step
+    A_i_pa = quaternion2dcm(q_i_pa); % rotation from inertial frame to PA
+
+    L_i_i(:, i) = A_i_pa' * L_i_pa(:, i); % transpose A because we want to go from PA to inertial
+    w_i_i(:, i) = A_i_pa' * w_i_pa(:, i); 
+end
+
+
+%% Plot them
+
+% Angular momentum over time
+figure 
+hold on
+plot(t_q, rad2deg(L_i_i(1,:)))
+plot(t_q, rad2deg(L_i_i(2,:)))
+plot(t_q, rad2deg(L_i_i(3,:)))
+title("Components of angular momentum in inertial coordinates")
+xlabel("Time, s")
+ylabel("Magnitude, deg/s")
+legend("L_1", "L_2", "L_3")
+ylim([-3 9])
+
+% Herpelhode trajectory in 3D
+figure
+hold on
+plot3(rad2deg(w_i_i(1,:)), rad2deg(w_i_i(2,:)), rad2deg(w_i_i(3,:)))
+view(3)
+quiver3( 0, 0, 0,  rad2deg(L_i_i(1,1)), rad2deg(L_i_i(2,2)),  rad2deg(L_i_i(3,3)))
