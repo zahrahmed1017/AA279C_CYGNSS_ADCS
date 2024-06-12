e98a3a705622ab77388238a20e587fb0d1b2f14e
Emily Bates <batesemily97@gmail.com>
2024-06-12 13:10:09 -0700

    switching to discrete control

--- Simulation_and_Propagation/ODEs/Control2ReacWheelTorque.m
+++ Simulation_and_Propagation/ODEs/Control2ReacWheelTorque.m
@@ -6,7 +6,7 @@ function Lw_d = Control2ReacWheelTorque(Lw, I_p, R_i_pa, w, A, Astar)
 
     % Calculate desired input control torque
     control_angs = [R_i_pa(2,3), -R_i_pa(1,3), R_i_pa(1,2)]'; % assume a 3-2-1 rotation
-    Mcontrol = controlTorque_inertial(I_p, control_angs, w);
+    Mcontrol = controlTorque_inertial_discrete(I_p, control_angs, w);
     
     % Calculate actuator torque 
     Lw_d = ComputeActuatorTorque(Lw, Mcontrol, w, A, Astar);
